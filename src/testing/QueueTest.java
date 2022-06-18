c1065520dc7b3b8e6af133c8861ac7c2704fb932
huynhloc-1110 <le1999loc@gmail.com>
2022-06-18 10:34:56 +0700

    Fixed untracked files

--- src/testing/QueueTest.java
+++ src/testing/QueueTest.java
@@ -1,24 +1,127 @@
 package testing;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Random;
 import dataStructure.Queue;
 
-public class QueueTest {
+class QueueTest {
 
-	public static void main(String[] args) {
-		Queue<String> queue = new Queue<>();
-		queue.printAllElements();
+	@Test
+	void testOfferAndPoll() {
+		/*
+		 * Offer numbers sequentially to queue.
+		 */
+		Queue<Integer> queue = new Queue<>();
+		for (int i = 1; i <= 10; i++) {
+			queue.offer(i);
+		}
 		
-		queue.offer("Loc");
-		queue.offer("Huy");
-		queue.offer("Khoa");
-		queue.offer("Khanh");
-
-		System.out.println(queue.poll());
-		System.out.println(queue.peek());
-		System.out.println(queue.size());
-		System.out.println(queue.isEmpty());
-		
-		queue.printAllElements();
+		/*
+		 * Poll numbers sequentially and check each time.
+		 */
+		for (int i = 1; i <= 10; i++) {
+			assertEquals(i, queue.poll());
+		}
 	}
+	
+	@Test
+	void testPeek() {
+		/*
+		 * Offer a random number to queue.
+		 * Check if the peek is that number.
+		 */
+		Queue<Integer> queue = new Queue<>();
+		Random random = new Random();
+		int r1 = random.nextInt(9);
+		queue.offer(r1);
+		assertEquals(r1, queue.peek());
+		
+		/*
+		 * Offer another random number to queue.
+		 * Check if the peek is the first number.
+		 */
+		int r2 = random.nextInt(9);
+		queue.offer(r2);
+		assertEquals(r1, queue.peek());
+	}
+	
+	@Test
+	void testSize() {
+		/*
+		 * Offer a random amount of numbers to the the queue.
+		 * Check the size of the queue.
+		 */
+		Queue<Integer> queue = new Queue<>();
+		Random random = new Random();
+		int length = random.nextInt(1, 20);
+		for (int i = 1; i <= length; i++) {
+			queue.offer(i);
+		}
+		assertEquals(length, queue.size());
+	}
+	
+	@Test
+	void testIsEmpty() {
+		/*
+		 * Generate the queue and ensure that it is empty.
+		 */
+		Queue<Integer> queue = new Queue<>();
+		assertTrue(queue.isEmpty());
 
+		/*
+		 * Offer a random amount of numbers to the the queue.
+		 * Ensure that the queue is not empty.
+		 */
+		Random random = new Random();
+		int length = random.nextInt(1, 20);
+		for (int i = 1; i <= length; i++) {
+			queue.offer(i);
+		}
+		assertFalse(queue.isEmpty());
+	}
+	
+	@Test
+	void testPrintAllElements() {
+		/*
+		 * Change output from console to the printsream.
+		 */
+		ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outputStreamCaptor));
+		
+		/*
+		 * Offer a random amount of elements to the queue.
+		 * Print all elements in the queue.
+		 */
+		Queue<Integer> queue = new Queue<>();
+		Random random = new Random();
+		int length = random.nextInt(1, 20);
+		for (int i = 1; i <= length; i++) {
+			queue.offer(i);
+		}
+		queue.printAllElements();
+		
+		/*
+		 * Generate expected outcome.
+		 */
+		String expected = "[ ";
+		for (int i = 1; i <= length; i++) {
+			expected += i + " ";
+		}
+		expected += "]";
+		
+		/*
+		 * Check.
+		 */
+		assertEquals(expected, outputStreamCaptor.toString().trim());
+		
+		/*
+		 * Change the output stream back to console.
+		 */
+		System.setOut(System.out);
+	}
+	
 }
--- src/testing/StackTest.java
+++ src/testing/StackTest.java
@@ -1,23 +1,126 @@
 package testing;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Random;
 import dataStructure.Stack;
 
-public class StackTest {
+class StackTest {
 
-	public static void main(String[] args) {
+	@Test
+	void testPushAndPop() {
+		/*
+		 * Push numbers sequentially to queue.
+		 */
 		Stack<Integer> stack = new Stack<>();
+		for (int i = 1; i <= 10; i++) {
+			stack.push(i);
+		}
+		
+		/*
+		 * Pop numbers sequentially and check each time.
+		 */
+		for (int i = 10; i >= 1; i--) {
+			assertEquals(i, stack.pop());
+		}
+	}
+	
+	@Test
+	void testPeek() {
+		/*
+		 * Push a random number to stack.
+		 * Check if the peek is that number.
+		 */
+		Stack<Integer> stack = new Stack<>();
+		Random random = new Random();
+		int r1 = random.nextInt(9);
+		stack.push(r1);
+		assertEquals(r1, stack.peek());
+		
+		/*
+		 * Push another random number to queue.
+		 * Check if the peek is the last number.
+		 */
+		int r2 = random.nextInt(9);
+		stack.push(r2);
+		assertEquals(r2, stack.peek());
+	}
+	
+	@Test
+	void testSize() {
+		/*
+		 * Push a random amount of numbers to the the stack.
+		 * Check the size of the queue.
+		 */
+		Stack<Integer> stack = new Stack<>();
+		Random random = new Random();
+		int length = random.nextInt(1, 20);
+		for (int i = 1; i <= length; i++) {
+			stack.push(i);
+		}
+		assertEquals(length, stack.size());
+	}
+	
+	@Test
+	void testIsEmpty() {
+		/*
+		 * Generate the stack and ensure that it is empty.
+		 */
+		Stack<Integer> stack = new Stack<>();
+		assertTrue(stack.isEmpty());
+
+		/*
+		 * Push a random amount of numbers to the the stack.
+		 * Ensure that the stack is not empty.
+		 */
+		Random random = new Random();
+		int length = random.nextInt(1, 20);
+		for (int i = 1; i <= length; i++) {
+			stack.push(i);
+		}
+		assertFalse(stack.isEmpty());
+	}
+	
+	@Test
+	void testPrintAllElements() {
+		/*
+		 * Change output from console to the printsream.
+		 */
+		ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outputStreamCaptor));
+		
+		/*
+		 * Push a random amount of elements to the stack.
+		 * Print all elements in the stack.
+		 */
+		Stack<Integer> stack = new Stack<>();
+		Random random = new Random();
+		int length = random.nextInt(1, 20);
+		for (int i = 1; i <= length; i++) {
+			stack.push(i);
+		}
 		stack.printAllElements();
 		
-		stack.push(4);
-		stack.push(5);
-		stack.push(6);
-		stack.push(7);
+		/*
+		 * Generate expected outcome
+		 */
+		String expected = "[ ";
+		for (int i = length; i >= 1; i--) {
+			expected += i + " ";
+		}
+		expected += "]";
 		
-		System.out.println(stack.pop());
-		System.out.println(stack.peek());
-		System.out.println(stack.size());
-		System.out.println(stack.isEmpty());
+		/*
+		 * Check.
+		 */
+		assertEquals(expected, outputStreamCaptor.toString().trim());
 		
-		stack.printAllElements();
+		/*
+		 * Change the output stream back to console.
+		 */
+		System.setOut(System.out);
 	}
 }
