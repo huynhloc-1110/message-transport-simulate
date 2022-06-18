5b93d29895d082fe2bdd9dfca5ce590b8767fa82
huynhloc-1110 <le1999loc@gmail.com>
2022-06-18 11:47:17 +0700

    Change time test to just calculate 1 time with repeating the process 1 million times.

--- src/testing/TimeComplexityTest.java
+++ src/testing/TimeComplexityTest.java
@@ -7,38 +7,30 @@ public class TimeComplexityTest {
 	public static void main(String[] args) {
 		Messager m1 = new Messager("M1");
 		Messager m2 = new Messager("M2");
-		
-		long[] timeList = new long[20];
+		int testTime = 1_000_000;
+		long testResult = 0;
 		
 		try {
 			m1.connect(m2);
-			for (int i = 0; i < timeList.length; i++) {
-				long startTime = System.currentTimeMillis();
-				
-				for (int j = 0; j < 100000; j++) {
-					m1.sendMessage("randomString");
-					m2.getMessage();
-					m2.displayMessages();
-				}
-				
-				long endTime = System.currentTimeMillis();
-				long elapsed = endTime - startTime;
-				timeList[i] = elapsed;
+			long startTime = System.currentTimeMillis();
+			
+			for (int i = 0; i < testTime; i++) {
+				m1.sendMessage("randomString");
+				m2.getMessage();
+				m2.displayMessages();
 			}
+			
+			long endTime = System.currentTimeMillis();
+			long elapsed = endTime - startTime;
+			testResult = elapsed;
+			m1.disconnect();
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
 			System.exit(1);
 		}
 		
-		System.out.println("Time recorded:");
-		long sum = 0;
-		for (long num : timeList) {
-			System.out.print(num + " ");
-			sum += num;
-		}
-		
-		System.out.println("\nAverage time: " + sum * 1.0 / timeList.length );
+		System.out.println("\nAverage time: " + testResult * 1.0 / testTime );
 	}
 
 }
